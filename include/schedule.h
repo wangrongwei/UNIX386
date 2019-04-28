fb5a9b9c69e41a4b13e61b37c20ab637eff7aa28
lollipop <wangrongwei2014@gmail.com>
2019-04-28 23:44:58 +0800

    correct movl to movw

--- include/schedule.h
+++ include/schedule.h
@@ -21,7 +21,7 @@ __asm__("movl %%esp,%%eax\n\t"\
 	"pushl $0x17\n\t" 	/* 压入ss */\
 	"pushl %%eax\n\t" 	/* 压入sp */\
 	"pushfl\n\t" 		/* 压入eflags */\
-	"movl $0x0f\n\t"	/* 压入cs */\
+	"pushl $0x0f\n\t"	/* 压入cs */\
 	"pushl $1f\n\t"		/* 压入ip */\
 	"iret\n\t"\
 	"1:\tmovl $0x17,%%eax\n\t"	/* RPL=3 TI=1(ldt) SEL=1 */\
