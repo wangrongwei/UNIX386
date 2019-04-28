77777833ad6f122e1eac99d6667e60081446bc03
lollipop <wangrongwei2014@gmail.com>
2019-04-28 23:12:06 +0800

    ...

--- kernel/sched.c
+++ kernel/sched.c
@@ -60,25 +60,5 @@ void reschedule(void)
 }
 
 
-/*
- * 改变进程权限：从0到3特权级 ss,sp,eflags,cs,ip
- */
-#define move_to_user_mode() \
-__asm__ ("movl %%esp,%%eax\n\t"\
-	"pushl $0x17\n\t"\ 	/* 压入ss */
-	"pushl %eax\n\t"\ 	/* 压入sp */
-	"pushfl\n\t"\ 		/* 压入eflags */
-	"movl $0x0f\n\t"\	/* 压入cs */
-	"pushl $1f\n\t"\	/* 压入ip */
-	"iret\n\t"\
-	"1:\tmovl $0x17,%%eax\n\t"\	/* RPL=3 TI=1(ldt) SEL=1 */
-	"movl %%ax,%%ds\n\t"\
-	"movl %%ax,%%es\n\t"\
-	"movl %%ax,%%fs\n\t"\
-	"movl %%ax,%%gs\n\t"\
-	 : : :"ax")
-
-
-
 
 
