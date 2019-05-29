10ec4d568a495283aa3cf055e9cbf2273a7e711e
lollipop <wangrongwei2014@gmail.com>
2019-05-30 01:30:45 +0800

    ...

--- init/interrupt.c
+++ init/interrupt.c
@@ -25,7 +25,7 @@ void isr_handler(pt_regs *regs)
 	else{
 		printk("unhandle interrupt_handler:%d\n",regs->int_no);
 #ifdef _DEBUG
-		console_clear();
+		//console_clear();
 		printk("ds:%d\n",regs->ds);		// 用于保存用户的数据段描述符
 		printk("edi:%d\n",regs->edi);		// 从 edi 到 eax 由 pusha 指令压入
 		printk("esi:%d\n",regs->esi);
