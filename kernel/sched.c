eaffd6e935abdcd05782a187ab0ffc4599f1d1e1
lollipop <wangrongwei2014@gmail.com>
2019-05-31 23:24:31 +0800

    ...

--- kernel/sched.c
+++ kernel/sched.c
@@ -34,9 +34,9 @@ void schedule_init(void)
 	/* 在gdt表后边加上进程0的tss和ldt */
 	//&(init_task.task.tss)
 	//&(init_task.task.ldt)
-	set_tssldt2_gdt(FIRST_TASKTSS_INDEX,0,0x89);
-	set_tssldt2_gdt(FIRST_TASKLDT_INDEX,0,0x82);
-	
+	set_tssldt2_gdt(FIRST_TASKTSS_INDEX,&(init_task.task.tss),0x89);
+	set_tssldt2_gdt(FIRST_TASKLDT_INDEX,&(init_task.task.ldt),0x82);
+	__asm__("pushfl ; andl $0xffffbfff,(%esp) ; popfl");
 	/* 将tss挂接到TR寄存器 */
 	ltr(0);
 	
