3b3b9181289cff34c16d3a94a76960fced532b25
lollipop <wangrongwei2014@gmail.com>
2019-06-03 20:13:00 +0800

    ...

--- include/schedule.h
+++ include/schedule.h
@@ -14,17 +14,19 @@ void reschedule(void);
 #define pg_dir (0)
 
 /*
- * 改变进程权限：从0到3特权级 ss,sp,eflags,cs,ip
+ * 改变进程权限：从0到3特权级 ss,sp,eflags,cs,ip "lret\n"\
+ * 压入ss sp eflags cs ip 
+ * movl $0x17,%%eax	RPL=3 TI=1(ldt) SEL=1
  */
 #define move_to_user_mode() \
 __asm__ __volatile__("movl %%esp,%%eax\n\t"\
-	"pushl $0x17\n\t" 	/* 压入ss */\
-	"pushl %%eax\n\t" 	/* 压入sp */\
-	"pushfl\n\t" 		/* 压入eflags */\
-	"pushl $0x0f\n\t"	/* 压入cs */\
-	"pushl $1f\n\t"		/* 压入ip */\
-	"lret\n"\
-	"1:\tmovl $0x17,%%eax\n\t"	/* RPL=3 TI=1(ldt) SEL=1 */\
+	"pushl $0x17\n\t"\
+	"pushl %%eax\n\t"\
+	"pushfl\n\t"\
+	"pushl $0x0f\n\t"\
+	"pushl $1f\n\t"\
+	"iret\n"\
+	"1:\tmovl $0x17,%%eax\n\t"\
 	"movw %%ax,%%ds\n\t" \
 	"movw %%ax,%%es\n\t" \
 	"movw %%ax,%%fs\n\t" \
