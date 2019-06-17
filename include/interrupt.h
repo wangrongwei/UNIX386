3053158e7f7005518c4d3e5027ad8e2e3803fc68
lollipop <wangrongwei2014@gmail.com>
2019-06-18 00:44:14 +0800

    ...

--- include/interrupt.h
+++ include/interrupt.h
@@ -7,24 +7,24 @@
  */
 
 
-// 寄存器类型
+/* 寄存器类型 */
 typedef struct pt_regs_t {
-	unsigned int ds;	// 用于保存用户的数据段描述符
-	unsigned int edi;	// 从 edi 到 eax 由 pusha 指令压入
-	unsigned int esi;
-	unsigned int ebp;
-	unsigned int esp;
-	unsigned int ebx;
-	unsigned int edx;
-	unsigned int ecx;
-	unsigned int eax;
-	unsigned int int_no;	// 中断号
-	unsigned int err_code;	// 错误代码(有中断错误代码的中断会由CPU压入)
-	unsigned int eip;	// 以下由处理器自动压入
-	unsigned int cs;
-	unsigned int eflags;
-	unsigned int useresp;
-	unsigned int ss;
+	long ds;	// 用于保存用户的数据段描述符
+	long edi;	// 从 edi 到 eax 由 pusha 指令压入
+	long esi;
+	long ebp;
+	long esp;
+	long ebx;
+	long edx;
+	long ecx;
+	long eax;
+	long int_no;	// 中断号
+	long err_code;	// 错误代码(有中断错误代码的中断会由CPU压入)
+	long eip;	// 以下由处理器自动压入
+	long cs;
+	long eflags;
+	long useresp;
+	long ss;
 } pt_regs;
 
 // 定义中断处理函数指针
