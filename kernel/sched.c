a79e3a39e690da13defd1ed4e33d5d973a587c4a
Rongwei Wang <wangrongwei2014@gmail.com>
2020-01-04 11:04:16 +0800

    set the busy-bit in TSS

--- kernel/sched.c
+++ kernel/sched.c
@@ -35,12 +35,11 @@ void schedule_init(void)
 {
 	printk("scheduler initial.\n");
 	/* 在gdt表后边加上进程0的tss和ldt */
-	set_tssldt2_gdt(FIRST_TASKTSS_INDEX,&(init_task.task.tss),0x89);
+	set_tssldt2_gdt(FIRST_TASKTSS_INDEX,&(init_task.task.tss),0x09);
 	set_tssldt2_gdt(FIRST_TASKLDT_INDEX,&(init_task.task.ldt),0x82);
 	__asm__ __volatile__("pushfl ; andl $0xffffbfff,(%esp) ; popfl");
 	/* 将tss挂接到TR寄存器 */
 	ltr(0);
-	set_tssldt2_gdt(FIRST_TASKTSS_INDEX,&(init_task.task.tss),0x8b);
 	/* 将LDT挂接到LDTR寄存器 */
 	lldt(0);
 
