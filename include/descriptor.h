709ba49eafbe6087d8939ab8340d503445af67e7
Rongwei Wang <wangrongwei2014@gmail.com>
2020-01-06 12:37:09 +0800

    ...

--- include/descriptor.h
+++ include/descriptor.h
@@ -232,10 +232,10 @@ static void init_gdt()
 	//set_gdt(2, 0, 0xfffff, P_SET&(~(DPL3 << 5))|S_NSS|TYPE_KERNEL_DS, 0x0c); //内核数据段
 	
 	set_gdt(3,0,0,0,0);//null
-	//set_gdt(4,0,0xfffff,0xfa,0x0c); //用户代码段-------| 进程0的TSS0（任务状态段）
-	//set_gdt(5,0,0xfffff,0xf2,0x0c); //用户数据段-------| 进程0的LDT0
-	set_gdt(4, 0, 0xfffff, P_SET|(DPL3 << 5)|S_NSS|TYPE_USER_CS, 0x0c); //用户代码段-------| 进程0的TSS0（任务状态段）
-	set_gdt(5, 0, 0xfffff, P_SET|(DPL3 << 5)|S_NSS|TYPE_USER_DS, 0x0c); //用户数据段-------| 进程0的LDT0
+	set_gdt(4,0,0xfffff,0xfa,0x0c); //用户代码段-------| 进程0的TSS0（任务状态段）
+	set_gdt(5,0,0xfffff,0xf2,0x0c); //用户数据段-------| 进程0的LDT0
+	//set_gdt(4, 0, 0xfffff, P_SET|(DPL3 << 5)|S_NSS|TYPE_USER_CS, 0x0c); //用户代码段-------| 进程0的TSS0（任务状态段）
+	//set_gdt(5, 0, 0xfffff, P_SET|(DPL3 << 5)|S_NSS|TYPE_USER_DS, 0x0c); //用户数据段-------| 进程0的LDT0
 	/* 后续的段描符初始化为0（与进程相关的代码） */
 	for(i=6;i<256;i++){
 		set_gdt(i,0,0,0,0);
